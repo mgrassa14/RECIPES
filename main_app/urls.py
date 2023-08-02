3582dd3e49d4efc2790b2aea013648daa25b24c3
Maria Grassa <mgrassa00gmail.com>
2023-08-02 15:52:47 -0700

    added css, home page, about page, view recipes page, & db

---
+++ main_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+# import all the functions from the views.py file and attach them to the variable named views
+from . import views
+
+urlpatterns = [
+    # localhost:8000
+    path('', views.home, name='home'),
+    # localhost:8000/about
+    path('about/', views.about, name='about'),
+    # localhost:8000/recipes
+    path('recipes/', views.recipes_index, name='index'),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+class Recipe:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, title, culture, description):
+    self.title = title
+    self.culture = culture
+    self.description = description
+
+recipes = [
+    Recipe('Pesto Pasta', 'italian', 'it is very good'),
+    Recipe('Bolognese Pasta', 'italiano', 'it is good'),
+    Recipe('Cabonara Pasta', 'italiana', 'it is the best')
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+# def recipes_index(request):
+#     recipes = Recipe.objects.all()
+#     return render(request, 'recipes/index.html', { 'recipes': recipes })
+def recipes_index(request):
+    return render(request, 'recipes/index.html', { 'recipes': recipes })
