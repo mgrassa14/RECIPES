3341b7becef710a7fe95e1cc3833c4e333faf320
Maria Grassa <mgrassa00gmail.com>
2023-08-15 09:11:10 -0700

    added working upload photos function

--- main_app/views.py
+++ main_app/views.py
@@ -11,7 +11,23 @@ from .forms import IngredientForm, DirectionForm
 S3_BASE_URL = 'https://s3.us-west-1.amazonaws.com/'
 BUCKET = 'recipeapp-8-14'
 
-# Create your views here.
+def add_photo(request, recipe_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = 'recipeApp' + uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to recipe_id or cat (if you have a cat object)
+            Photo.objects.create(url=url, recipe_id=recipe_id)
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', recipe_id=recipe_id)
 
 def add_ingredient(request, recipe_id):
     form = IngredientForm(request.POST)
